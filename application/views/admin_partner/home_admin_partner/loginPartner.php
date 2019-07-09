c0ab51696546d1371b2909742d8430e42918abb6
Kiều Hữu Thành <34344168+kieuhuuthanh23698@users.noreply.github.com>
2019-07-09 08:25:47 +0700

    page login partner

---
+++ application/views/admin_partner/home_admin_partner/loginPartner.php
@@ -0,0 +1,86 @@
+<html>
+<head> 
+	<meta charset="utf-8"> 
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Đăng nhập tài khoản -  HMS Hệ thống quản lý khách sạn trực tuyến</title>
+	<meta name="robots" content="noindex, nofollow"> 
+	<link rel="stylesheet" href="https://betahms.mytour.vn/dist/app.2d290b316b5a2f7d1e30.css" type="text/css"> 
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Quicksand:500,700&amp;subset=vietnamese" type="text/css">  
+</head> 
+<body class="hold-transition login-page skin-blue">  
+	<video id="bgvid" playsinline="" autoplay="" muted="" loop="">
+		<source src="https://s3-ap-southeast-1.amazonaws.com/mytourcdn.com/video/bg-video-mp4.mp4" type="video/mp4"> </video> <div class="video-bg"></div>   <div class="login-box">
+			<div class="login-box-body">
+				<h1>ĐĂNG NHẬP</h1> 
+				<p class="login-box-msg" style="color: #8a98a6">Chưa có tài khoản? 
+					<a href="http://betahms.mytour.vn/join-with-us" class="text-blue" style="font-weight: 700">Đăng ký</a> ngay</p>
+					<form method="POST" action="http://betahms.mytour.vn/auth/login">
+						<input type="hidden" name="_token" value="aqdfKcLtvLEbyntVDn8cyrbKE92RKdtNEwULnyNb"> 
+						<div class="form-group has-feedback "> 
+							<label style="color:#3f556b">TÀI KHOẢN</label>
+							<input type="text" name="use_login" value="" autofocus="" class="form-control"> 
+							<span class="fa fa-user form-control-feedback" style="color: #eaecef"></span> 
+						</div>
+						<div class="form-group has-feedback "> 
+							<label style="color:#3f556b">MẬT KHẨU</label> 
+							<input type="password" name="password" class="form-control">
+							<span class="fa fa-lock form-control-feedback" style="color: #eaecef"></span> 
+						</div> 
+						<div class="row"> 
+							<div class="col-xs-7"> 
+								<div class="checkbox" style="margin-top: 0; margin-bottom: 24px;"> 
+									<label>
+										<input type="checkbox" name="remember"> 
+										<span style="color:#8a98a6">Lưu tài khoản</span>
+									</label>
+								</div> 
+							</div> 
+							<div class="col-xs-5">
+								<a href="http://betahms.mytour.vn/auth/password/reset" class="text-red pull-right"> Quên mật khẩu</a> 
+							</div> 
+							<div class="col-xs-12">
+								<button type="submit" class="btn btn-primary btn-block btn-flat">ĐĂNG NHẬP</button>
+							</div>
+						</div> 
+					</form>
+				</div> 
+			</div> 
+			<div class="login-box-footer row mar-top-12"> 
+				<div class="single-partner">
+					<img src="dist/img/partner-06.png" alt="">
+				</div> 
+				<div class="single-partner">
+					<img src="dist/img/partner-09.png" alt="">
+				</div> 
+				<div class="single-partner">
+					<img src="dist/img/partner-04.png" alt="">
+				</div> 
+				<div class="single-partner">
+					<img src="dist/img/partner-11.png" alt="">
+				</div> 
+				<div class="single-partner">
+					<img src="dist/img/partner-07.png" alt="">
+				</div> 
+				<div class="single-partner">
+					<img src="dist/img/partner-10.png" alt="">
+				</div> 
+				<div class="single-partner">
+					<img src="dist/img/partner-03.png" alt="">
+				</div>
+				<div class="single-partner">
+					<img src="dist/img/partner-05.png" alt="">
+				</div> 
+				<div class="single-partner">
+					<img src="dist/img/partner-08.png" alt="">
+				</div>
+				<div class="single-partner">
+					<img src="dist/img/partner-02.png" alt="">
+				</div> 
+			</div>
+			<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+			<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" type="text/javascript"></script>  
+		</body>
+		<div style="position: absolute; top: 0px;">
+
+		</div>
+		</html>
