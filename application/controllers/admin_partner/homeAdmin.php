8f85c387348e4bf8533c7a3327df3855f30727cb
thanhcong1112101411 <34175974+thanhcong1112101411@users.noreply.github.com>
2019-07-14 17:35:43 +0700

    a

--- application/controllers/admin_partner/homeAdmin.php
+++ application/controllers/admin_partner/homeAdmin.php
@@ -14,7 +14,6 @@ class homeAdmin extends CI_Controller {
 
 	public function index()
 	{
-		$id = $this->session->userdata('partner')[0]['id_destination'];
         $data['partner'] = $this->M_data->load_query("select * from destination where id_destination = ".$id)[0]; 
         $view['body'] = $this->load->view('admin_partner/pages_admin_partner/inforPartner', $data, TRUE);
         $this->load->view('admin_partner/home_admin_partner/masterAdminPartner', $view);
