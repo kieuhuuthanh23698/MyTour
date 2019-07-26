5e31eca9e0a0313e0f250d3bb90c798c7f1bc7d2
thanhcong <thanhcong1112101411@gmail.com>
2019-07-26 21:47:56 +0700

    aaa

---
+++ application/views/home/footer.php
@@ -0,0 +1,49 @@
+<div class="footer mt-5">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 footer-style">
+                <h1>Công ty tnhh mytour việt nam</h1>
+                <ul>
+                    <li>Hà Nội: 024 7109 9999</li>
+                    <li>Hồ Chí Minh: 028 7109 9998</li>
+                    <li>Email: admin@mytour.vn</li>
+                    <li>Văn phòng Hà Nội: Tầng 4, tòa nhà GP Invest, 170 Đê La Thành, Đống Đa</li>
+                    <li>Văn phòng HCM: Tầng 5, tòa nhà HDTC, số 36 Bùi Thị Xuân, Q.1</li>
+                </ul>
+            </div>
+            <div class="col-md-4 footer-style">
+                <h1>Quy Định Dành cho website</h1>
+                <ul>
+                    <li>Chính sách và quy định chung</li>
+                    <li>Quy định về thanh toán</li>
+                    <li>Quy định về xác nhận thông tin đặt phòng</li>
+                    <li>Chính sách về hủy đặt phòng và hoàn trả tiền</li>
+                    <li>Chính sách bảo mật thông tin</li>
+                </ul>
+            </div>
+            <div class="col-md-4 footer-style">
+                <h1>Khách hàng và đối tác</h1>
+                <ul>
+                    <li><a href="#">Tích lũy Vpoint</a></li>
+                    <li><a href="<?php echo base_url()?>admin_partner/loginPartner">Đăng nhập HMS</a></li>
+                    <li><a href="<?php echo base_url()?>admin_partner/signinPartner">Đối tác khách sạn</a></li>
+                    <li><a href="#">Liên Hệ</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<style>
+    .footer-style h1{
+        font-size: 30px;
+        text-transform: capitalize;
+    }
+    .footer-style ul{
+        list-style: none;
+        margin-left: 0;
+        padding: 0;
+    }
+    .footer-style a{
+        text-decoration: none;
+    }
+</style>
